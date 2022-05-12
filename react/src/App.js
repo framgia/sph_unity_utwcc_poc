a629daebe111fd5391b188c83210856308117b1a
TheRedLegz <redempto.legaspi.20@usjr.edu.ph>
2022-05-12 18:55:34 +0800

    [TASK] Web UI Layout - Removed Dark Layout

--- react/src/App.js
+++ react/src/App.js
@@ -20,32 +20,32 @@ function App() {
 
   return (
     <div className="flex justify-between" style={{ height: "100vh", width: "100vw" }}>
-      <div className="w-full p-6 sm:w-60 dark:bg-gray-900 dark:text-gray-100">
+      <div className="w-full p-6 sm:w-60 bg-coolGray-900 text-coolGray-100">
         <nav className="space-y-8 text-sm">
 
           <div className="space-y-2">
-            <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">Image</h2>
+            <h2 className="text-sm font-semibold tracking-widest uppercase text-coolGray-400">Image</h2>
             <div className="flex flex-col space-y-1">
-              <button type="button" className="px-8 py-3 font-semibold rounded hover:bg-gray-200 dark:bg-gray-100 dark:text-gray-800">Webcam Capture</button>
+              <button type="button" className="px-8 py-3 font-semibold rounded hover:bg-coolGray-200 bg-coolGray-100 text-coolGray-800">Webcam Capture</button>
             </div>
           </div>
 
           <div className="space-y-2">
-            <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">Video</h2>
+            <h2 className="text-sm font-semibold tracking-widest uppercase text-coolGray-400">Video</h2>
             <div className="flex flex-col space-y-1">
-              <button type="button" className="px-8 py-3 font-semibold rounded hover:bg-gray-200 dark:bg-gray-100 dark:text-gray-800">Live Capture</button>
+              <button type="button" className="px-8 py-3 font-semibold rounded hover:bg-coolGray-200 bg-coolGray-100 text-coolGray-800">Live Capture</button>
             </div>
           </div>
 
           <div className="space-y-2">
-            <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">Audio</h2>
+            <h2 className="text-sm font-semibold tracking-widest uppercase text-coolGray-400">Audio</h2>
             <div className="flex flex-col space-y-1">
-              <button type="button" className="px-8 py-3 font-semibold rounded hover:bg-gray-200 dark:bg-gray-100 dark:text-gray-800">Record</button>
+              <button type="button" className="px-8 py-3 font-semibold rounded hover:bg-coolGray-200 bg-coolGray-100 text-coolGray-800">Record</button>
             </div>
           </div>
 
           <div className="space-y-2">
-            <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">Upload media</h2>
+            <h2 className="text-sm font-semibold tracking-widest uppercase text-coolGray-400">Upload media</h2>
             <div className="flex flex-col space-y-1">
               <input type="file" id="media" name="media"
                 accept="image/png, image/jpeg, video/mp4, video/mkv, video/*, audio/*" />
@@ -53,7 +53,7 @@ function App() {
           </div>
 
           <div className="space-y-2">
-            <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">Shape</h2>
+            <h2 className="text-sm font-semibold tracking-widest uppercase text-coolGray-400">Shape</h2>
             <div className="flex flex-col space-y-1">
               <label htmlFor="range" className="font-bold text-gray-600">Size: {shapeSize}</label>
               <input type="range" name="shape" step="1" min="0" max={maxShapeSize} defaultValue={shapeSize} className="w-full h-2 bg-blue-100 appearance-none" onChange={handleShapeSlider}/>
@@ -61,7 +61,7 @@ function App() {
           </div>
 
           <div className="space-y-2">
-            <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">Color Picker</h2>
+            <h2 className="text-sm font-semibold tracking-widest uppercase text-coolGray-400">Color Picker</h2>
             <div className="flex flex-col space-y-1">
               <label htmlFor="colorpicker">Select:</label>
               <input type="color" id="colorpicker" name="colorpicker" value="#ff0000" />
