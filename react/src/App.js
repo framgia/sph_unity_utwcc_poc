2c8d623e49c5dd8afbdd940a5fc356f483d6b0d4
sonrhey27 <sonrhey.deiparine@sun-asterisk.com>
2022-05-25 11:43:18 +0800

    Update Max size

--- react/src/App.js
+++ react/src/App.js
@@ -167,7 +167,7 @@ function App() {
             <h2 className="text-sm font-semibold tracking-widest uppercase text-coolGray-400">Shape</h2>
             <div className="flex flex-col space-y-1">
               <label htmlFor="range" className="font-bold text-gray-600">Size: {shapeSize}</label>
-              <input type="range" name="shape" step="1" min="0" max="6" defaultValue="3" className="w-full h-2 bg-blue-100 appearance-none" onChange={handleShapeSlider}/>
+              <input type="range" name="shape" step="1" min="0" max="4" defaultValue="3" className="w-full h-2 bg-blue-100 appearance-none" onChange={handleShapeSlider}/>
             </div>
           </div>
 
